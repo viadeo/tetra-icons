e29303ebe1f9ebb208f0e18383026ffe9a011a8e
Duvillier Antoine <duvillierantoine@gmail.com>
2015-10-29 17:00:45 +0100

    Simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -38,55 +38,32 @@ const DIST = {
 
 gulp.task('iconfont', function () {
 
-  // Localisation des fichiers SVG
-  gulp.src(LIB.svg)
-    // Appel du module générant la police d'icône
-    .pipe(iconfont({
-    // Nom de la police
+  const IconfontOptions = {
     fontName: FONT.name
-    // Calcule des dimensions du glyphe et centrage
     , centerHorizontally: true
-    // Normalisation des icônes par mise à l'échelle
-    // par rapport à la taille de l'icône la plus grande
     , normalize: true
-    // Assigne un unicode à chaque icône pour les utiliser dans du CSS
     , appendUnicode: true
     , timestamp: runTimestamp // recommended to get consistent builds when watching files
-    // If some font glyphs aren't converted properly you should append the normalize:true option and a fontHeight greater than 1000 (fontHeight: 1001).
-    // , fontHeight: 1001
-    }))
+  };
 
-    // Appel du module générant le CSS
+  gulp.src(LIB.svg)
+    .pipe(iconfont(IconfontOptions))
     .on('glyphs', function (glyphs) {
-      // Localisation du template SASS
-      gulp.src('lib/scss/templates/_viadeoicons.scss')
-        // Appel du moteur de template
-        .pipe(consolidate('lodash', {
-          // Code points présent dans la propriété CSS "content"
+      const templateParams = {
           glyphs: glyphs
-          // Nom de la police
         , fontName: FONT.name
-        // Chemin des fichiers de police
         , fontPath: FONT.path
-        // Nom de la classe principale, commune à tous les icônes
         , className: FONT.className
-        }))
-        // Destination du fichier SASS qui sera ensuite générer en CSS
+      };
+      gulp.src('lib/scss/templates/_'+FONT.name+'.scss')
+        .pipe(consolidate('lodash', templateParams))
         .pipe(gulp.dest(LIB.root+'scss'))
         .pipe(gulp.dest(DIST.scss));
 
-        // Localisation du template HTML
-        gulp.src(LIB.html)
-          // Appel du moteur de template
-          .pipe(consolidate('lodash', {
-            glyphs: glyphs
-          , className: FONT.className
-          }))
-          .pipe(gulp.dest(DIST.root));
-
+      gulp.src(LIB.html)
+        .pipe(consolidate('lodash', templateParams))
+        .pipe(gulp.dest(DIST.root));
     })
-
-    // Destination des fichiers de police
     .pipe(gulp.dest(DIST.fonts));
 });
 
